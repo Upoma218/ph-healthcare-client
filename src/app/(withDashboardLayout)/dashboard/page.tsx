d845c8b342b41ee4b50ad70f6d86de21ba15f722
Upoma21801~ <upoma.cdsr@gmail.com>
2024-05-27 04:35:04 +0600

    redux configuration done

---
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardHomePage = () => {
+  return (
+    <div>
+      <h1>Welcome To Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
---
+++ src/app/(withDashboardLayout)/dashboard/super_admin/page.tsx
@@ -0,0 +1,9 @@
+const SuperAdminPage = () => {
+  return (
+    <div>
+      <h1>SuperAdminDashboard</h1>
+    </div>
+  );
+};
+
+export default SuperAdminPage;
